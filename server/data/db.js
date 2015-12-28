2b0c726da8430e37cc120381a36bd4bbe48912ff
Julien Viala <dev.vialajulien@gmail.com>
2015-12-28 16:44:17 +0100

    fix the way db assign new id's to selfies

--- server/data/db.js
+++ server/data/db.js
@@ -1,23 +1,22 @@
 import find from 'lodash.find';
 
+var selfieNextId = 0;
 var selfies = [
     {
-        id: 0,
         author: 'Kim',
         src: 'http://img1.closermag.fr/var/closermag/storage/images/media/images-des-contenus/actu-people/people-anglo-saxons/20150830-kim/kim-kardashian-en-vacances-a-st-barth-sur-instagram/4834046-1-fre-FR/Kim-Kardashian-en-vacances-a-St-Barth-sur-Instagram_exact1024x768_l.jpg',
         likesCount: 9525251
     },
     {
-        id: 1,
         author: 'Leo',
         src: 'https://i.ytimg.com/vi/cHgIQYvv9kk/hqdefault.jpg'
     }
 ];
 
-selfies = selfies.map(obj => new Selfie(obj.id, obj.author, obj.src, obj.likesCount));
+selfies = selfies.map(data => new Selfie(data));
 
-function Selfie(id, author, src, likesCount = 0) {
-    this.id = id;
+function Selfie({ author, src, likesCount = 0 }) {
+    this.id = `${selfieNextId++}`;
     this.author = author;
     this.src = src;
     this.likesCount = likesCount;
@@ -28,20 +27,24 @@ function User(id) {
 }
 
 const defaultUser = new User('me');
+const getUser = () => defaultUser;
+const getSelfies = () => selfies;
+const getSelfie = (id) => find(selfies, { id: id });
+const addSelfie = (data) => {
+
+    let newSelfie = new Selfie(data);
+
+    selfies.unshift(newSelfie);
+
+    return newSelfie.id;
+};
 
 export default {
     Selfie,
     User,
 
-    getUser: () => defaultUser,
-    getSelfies: () => selfies,
-    getSelfie: (id) => find(selfies, { id: id }),
-    addSelfie: (author, src) => {
-        
-        selfies.unshift( new Selfie(
-            selfies.length + 1,
-            author,
-            src
-        ))
-    }
+    getUser,
+    getSelfies,
+    getSelfie,
+    addSelfie
 }
