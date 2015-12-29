7802bc0c66a981c1ad3c5d0ecc3a6d0081c7119a
Julien Viala <dev.vialajulien@gmail.com>
2015-12-29 15:06:07 +0100

    more logic into app for starting selfie

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -3,7 +3,9 @@ import CSSModules from 'react-css-modules';
 import styles from 'styles/App.css';
 import Login from './LoginComponent';
 import Header from './parts/HeaderComponent';
-import { loggedIn } from '../AppState';
+import SelfieButton from './parts/SelfieButtonComponent';
+import HiddenForm from './parts/HiddenFormComponent';
+import { loggedIn, getAppState, updateAppState } from '../AppState';
 
 class AppComponent extends React.Component {
 
@@ -11,25 +13,47 @@ class AppComponent extends React.Component {
         this.context.history.pushState(null, 'feed');
     }
 
+    handleSelfieButton() {
+        this.refs.HiddenForm.emulateClick();
+    }
+
+    handleImageChanged() {
+        this.context.history.pushState({
+            imageData: this.refs.HiddenForm.getFile()
+        }, 'selfie');
+    }
+
     get composedChildren() {
 
+        const { location: { pathname } } = this.props;
+
         return (
-            <div styleName="wrapper">
+            <div styleName="main-wrapper">
+
                 <Header />
 
                 <div styleName="page-wrapper">
                     {this.props.children}
+
+                    {pathname === 'feed' && <SelfieButton takeSelfieHandler={this.handleSelfieButton.bind(this)} />}
                 </div>
+
             </div>
         )
     }
 
     render() {
 
+        const _loggedIn = loggedIn();
+
         return (
             <div styleName="root">
 
-                {(loggedIn() && this.composedChildren) || <Login loginHandler={this.handleLogin.bind(this)} />}
+                {(_loggedIn && this.composedChildren) || <Login loginHandler={this.handleLogin.bind(this)} />}
+
+                {_loggedIn &&
+                    <HiddenForm ref="HiddenForm" handleImageChanged={this.handleImageChanged.bind(this)} />
+                }
 
             </div>
         );
