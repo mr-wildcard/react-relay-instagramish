475f11a314c59303bd98e34973de2123b303b00b
Julien Viala <dev.vialajulien@gmail.com>
2015-12-30 14:31:23 +0100

    TODO: file upload

--- server/server.js
+++ server/server.js
@@ -3,13 +3,19 @@
 import koa from 'koa';
 import mount from 'koa-mount';
 import cors from 'koa-cors';
+import route from 'koa-route';
+import multer from 'koa-multer';
+import multerWrapper from './helpers/multer';
 import graphqlHTTP from 'koa-graphql';
 import graphQLSchema from './data/schema';
 
 let app = koa();
+let storage = multer.memoryStorage();
 
 app.use(cors());
 
+// https://github.com/chentsulin/koa-graphql/blob/master/src/__tests__/http-test.js#L600
+// app.use(mount('/graphql', multerWrapper({ storage }).single('file')));
 app.use(
     mount('/graphql', graphqlHTTP({
         schema: graphQLSchema,
