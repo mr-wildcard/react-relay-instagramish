0e54225d65b1d9fc932f6e2b63e8ae1ff4aea1f5
Julien Viala <dev.vialajulien@gmail.com>
2016-01-04 16:04:54 +0100

    clean up old code

--- server/server.js
+++ server/server.js
@@ -4,20 +4,13 @@ import koa from 'koa';
 import mount from 'koa-mount';
 import cors from 'koa-cors';
 import multer from 'koa-multer';
-import koaBody from 'koa-body';
-import cloud from './helpers/cloud';
 import graphqlHTTP from 'koa-graphql';
 import graphQLSchema from './data/schema';
 
 let app = koa();
-//let storage = multer.memoryStorage();
 
 app.use(cors());
 
-// https://github.com/chentsulin/koa-graphql/blob/master/src/__tests__/http-test.js#L600
-// app.use(mount('/graphql', multerWrapper({ storage }).single('file')));
-app.use(koaBody({ limit: 5000 * 1024 }));
-app.use(mount('/graphql', cloud()));
 app.use(
     mount('/graphql', graphqlHTTP({
         schema: graphQLSchema,
